fcabc207cc74d6eccdd218412d43aea0e66a5d10
golden-hamster <isack19999@gmail.com>
2023-11-28 10:25:56 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/hello/helloprojectboard/HelloProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hello.helloprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloProjectBoardApplication.class, args);
+	}
+
+}

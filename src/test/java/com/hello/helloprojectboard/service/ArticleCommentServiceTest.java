0bb72ebbc62a09795b59469814fd5b5047edcdc9
golden-hamster <isack19999@gmail.com>
2024-01-03 19:12:45 +0900

    #42 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/test/java/com/hello/helloprojectboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/hello/helloprojectboard/service/ArticleCommentServiceTest.java
@@ -146,7 +146,6 @@ class ArticleCommentServiceTest {
 
     private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
-                1L,
                 "Isack",
                 "password",
                 "Isack@mail.com",

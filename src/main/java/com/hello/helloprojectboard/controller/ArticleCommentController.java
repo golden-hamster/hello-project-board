ff61d4b6f65128a6247331ecbdb669d651807f4b
golden-hamster <isack19999@gmail.com>
2024-01-10 22:04:00 +0900

    #34 - 시큐리티 설정과 자바 코드 구현
    스프링부트 가이드에 맞춰서
    시큐리티 설정을 적용하고, 인증 정보를 db로부터 부르는 빈과 패스워드 인코더 빈까지 등록
    영향받는 부분을 서비스 코드와 컨트롤러에 반영

--- src/main/java/com/hello/helloprojectboard/controller/ArticleCommentController.java
+++ src/main/java/com/hello/helloprojectboard/controller/ArticleCommentController.java
@@ -3,8 +3,10 @@ package com.hello.helloprojectboard.controller;
 import com.hello.helloprojectboard.dto.UserAccountDto;
 import com.hello.helloprojectboard.dto.request.ArticleCommentRequest;
 import com.hello.helloprojectboard.dto.request.ArticleRequest;
+import com.hello.helloprojectboard.dto.security.BoardPrincipal;
 import com.hello.helloprojectboard.service.ArticleCommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,18 +20,20 @@ public class ArticleCommentController {
     private final ArticleCommentService articleCommentService;
 
     @PostMapping("/new")
-    public String postNewArticleComment(ArticleCommentRequest articleCommentRequest) {
+    public String postNewArticleComment(ArticleCommentRequest articleCommentRequest,
+                                        @AuthenticationPrincipal BoardPrincipal boardPrincipal
+                                        ) {
 
-        articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
-                "isack", "pw", "isack@mail.com", null, null
-        )));
+        articleCommentService.saveArticleComment(articleCommentRequest.toDto(boardPrincipal.toDto()));
 
         return "redirect:/articles/" + articleCommentRequest.articleId();
     }
 
     @PostMapping ("/{commentId}/delete")
-    public String deleteArticleComment(@PathVariable Long commentId, Long articleId) {
-        articleCommentService.deleteArticleComment(commentId);
+    public String deleteArticleComment(@PathVariable Long commentId,
+                                       @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+                                       Long articleId) {
+        articleCommentService.deleteArticleComment(commentId, boardPrincipal.getUsername());
         return "redirect:/articles/" + articleId;
     }
 }
--- src/main/java/com/hello/helloprojectboard/controller/ArticleController.java
+++ src/main/java/com/hello/helloprojectboard/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.hello.helloprojectboard.controller;
 
+import com.hello.helloprojectboard.dto.security.BoardPrincipal;
 import com.hello.helloprojectboard.domain.constant.FormStatus;
 import com.hello.helloprojectboard.domain.constant.SearchType;
 import com.hello.helloprojectboard.dto.UserAccountDto;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -53,8 +55,8 @@ public class ArticleController {
 
     @GetMapping("/search-hashtag")
     public String searchArticleHashtag(@RequestParam(required = false) String searchValue,
-                                @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-                                ModelMap map) {
+                                       @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+                                       ModelMap map) {
         Page<ArticleResponse> articles = articleService.searchArticlesViaHashtag(searchValue, pageable).map(ArticleResponse::from);
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
         List<String> hashtags = articleService.getHashtags();
@@ -75,11 +77,9 @@ public class ArticleController {
     }
 
     @PostMapping("/form")
-    public String postNewArticle(ArticleRequest articleRequest) {
-        // TODO: 인증 정보를 넣어줘야 한다.
-        articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "isack", "asdf1234", "isack@mail.com", "Isack", "memo"
-        )));
+    public String postNewArticle(ArticleRequest articleRequest,
+                                 @AuthenticationPrincipal BoardPrincipal boardPrincipal) {
+        articleService.saveArticle(articleRequest.toDto(boardPrincipal.toDto()));
 
         return "redirect:/articles";
     }
@@ -94,20 +94,19 @@ public class ArticleController {
         return "articles/form";
     }
 
-    @PostMapping ("/{articleId}/form")
-    public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
-        // TODO: 인증 정보를 넣어줘야 한다.
-        articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "isack", "asdf1234", "isack@mail.com", "Isack", "memo"
-        )));
+    @PostMapping("/{articleId}/form")
+    public String updateArticle(@PathVariable Long articleId,
+                                @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+                                ArticleRequest articleRequest) {
+        articleService.updateArticle(articleId, articleRequest.toDto(boardPrincipal.toDto()));
 
         return "redirect:/articles/" + articleId;
     }
 
-    @PostMapping ("/{articleId}/delete")
-    public String deleteArticle(@PathVariable Long articleId) {
-        // TODO: 인증 정보를 넣어줘야 한다.
-        articleService.deleteArticle(articleId);
+    @PostMapping("/{articleId}/delete")
+    public String deleteArticle(@PathVariable Long articleId,
+                                @AuthenticationPrincipal BoardPrincipal boardPrincipal) {
+        articleService.deleteArticle(articleId, boardPrincipal.getUsername());
 
         return "redirect:/articles";
     }

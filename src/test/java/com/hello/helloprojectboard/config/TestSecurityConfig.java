b79b87228e99daa903487f7d72352a0aef27b90d
golden-hamster <isack19999@gmail.com>
2024-01-28 02:38:05 +0900

    #78 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/test/java/com/hello/helloprojectboard/config/TestSecurityConfig.java
+++ src/test/java/com/hello/helloprojectboard/config/TestSecurityConfig.java
@@ -1,7 +1,8 @@
 package com.hello.helloprojectboard.config;
 
-import com.hello.helloprojectboard.domain.UserAccount;
+import com.hello.helloprojectboard.dto.UserAccountDto;
 import com.hello.helloprojectboard.repository.UserAccountRepository;
+import com.hello.helloprojectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -15,15 +16,23 @@ import static org.mockito.BDDMockito.given;
 public class TestSecurityConfig {
 
     @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetup() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createuserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createuserAccountDto());
+    }
+
+    private UserAccountDto createuserAccountDto() {
+        return UserAccountDto.of(
                 "isackTest",
                 "pw",
                 "isack-test@email.com",
                 "isack-test",
                 "test memo"
-        )));
+        );
     }
 }

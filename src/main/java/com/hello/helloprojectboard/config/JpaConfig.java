2771bb8f0307850f405edfa409943a8b44a5cd8c
golden-hamster <isack19999@gmail.com>
2024-02-15 23:54:42 +0900

    #89 - JpaConfig: AuditorAware 수정
    회원가입시에는 인증받지 않은 사용자가 요청을 하기 때문에, `Authentication::getPrincipal` 에서 String 타입인 `anonymouseUser` 가 반환되면서 `BoardPrincipal.class::cast` 에서 casting 이 안돼서 에러가 뜸. 이에 수정함

--- src/main/java/com/hello/helloprojectboard/config/JpaConfig.java
+++ src/main/java/com/hello/helloprojectboard/config/JpaConfig.java
@@ -15,14 +15,31 @@ import java.util.Optional;
 @Configuration
 public class JpaConfig {
 
+//    @Bean
+//    public AuditorAware<String> auditorAware() {
+//        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+//                .map(SecurityContext::getAuthentication)
+//                .filter(Authentication::isAuthenticated)
+//                .map(Authentication::getPrincipal)
+//                .map(BoardPrincipal.class::cast)
+//                .map(BoardPrincipal::getUsername);
+//    }
+
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
-                .map(SecurityContext::getAuthentication)
-                .filter(Authentication::isAuthenticated)
-                .map(Authentication::getPrincipal)
-                .map(BoardPrincipal.class::cast)
-                .map(BoardPrincipal::getUsername);
-    }
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//                 인증되지 않은 사용자인 경우
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.empty();
+            }
 
+//                 인증된 사용자인 경우
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof BoardPrincipal) {
+                return Optional.of(((BoardPrincipal) principal).getUsername());
+            }
+            return Optional.empty();
+        };
+    }
 }
